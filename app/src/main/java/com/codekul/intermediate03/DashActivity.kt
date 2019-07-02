c1c5e98604bc917f887dacbaa781e39c6e5e65a8
stuty1997 <bhargava.stuty@gmail.com>
2019-07-02 12:51:33 +0530

    intermediate of two activities

---
+++ app/src/main/java/com/codekul/intermediate03/DashActivity.kt
@@ -0,0 +1,27 @@
+package com.codekul.intermediate03
+
+import android.app.Activity
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_dash.*
+class DashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dash)
+        val mnm= intent.extras?.getString(MainActivity.KEY_NAME)
+        txtRs.text= mnm
+        btBk.setOnClickListener {
+            val resInt = Intent()
+            val bnd = Bundle()
+            bnd.putString(KEY_RES,doProcessing())
+            resInt.putExtras(bnd)
+            setResult(Activity.RESULT_OK,resInt)
+            finish()
+        }
+    }
+    fun doProcessing()= txtRs.text.toString().toUpperCase()
+    companion object{
+        const val KEY_RES = "result_data"
+    }
+}
---
+++ app/src/main/java/com/codekul/intermediate03/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.codekul.intermediate03
+
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.ActionMode
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        btnNxt.setOnClickListener {
+            val int=Intent(this@MainActivity,
+                DashActivity::class.java
+            )
+            val bnd=Bundle()
+            bnd.putString("KEY_NAME",etTx.text.toString())
+            int.putExtras(bnd)
+            startActivityForResult(int,REQ_DASH)
+            //startActivity(int)
+            finish()
+        }
+}
+    companion object {
+    const val KEY_NAME ="my_name"
+        const val REQ_DASH ="4568"
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == REQ_DASH) {
+            if (resultCode == Activity.RESULT_OK) {
+                val processedDash = data?.extras?.getString(DashActivity.KEY_RES)
+            }
+    }
+    }
+}
